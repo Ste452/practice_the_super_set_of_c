315046799d811e512f24afd153b467a372a04019
Steve <falecomstevevaz@gmail.com>
2025-11-20 10:25:18 -0300

    Arithmetic Attribution Operators

--- Part_I_Introdution/section_2_Basic_operators/ArithmeticAttributionOperators.cpp
+++ Part_I_Introdution/section_2_Basic_operators/ArithmeticAttributionOperators.cpp
@@ -11,11 +11,17 @@ int main () {
     cin >> number2;
     number1 += number2; // number1 = number1 + number2; 
     cout << "Addition: ";
-    cout << number2 << "->" << number1 << endl;
+    cout << number2 << "-> " << number1 << endl;
     number1 -= number2; // number1 = number1 - number2;
     cout << "Subtraction: ";
-    cout << number2 << "->" << number1 << endl;
+    cout << number2 << "-> " << number1 << endl;
     number1 *= number2; // number1 = number1 * number2;
     cout << "Multiplication: ";
+    cout << number2 << "-> " << number1 << endl;
+    number1 /= number2; // number1 = number1 / number2;
+    cout << "Division: ";
+    cout << number2 << "-> " << number1 << endl;
+    number1 %= number2; // number1 = number1 % number2;
+    cout << "Division with remainder: ";
     cout << number2 << "->" << number1 << endl;
 }   
